7eba5bd7f40358994ff9d186d5b259d894d3babc
diegonieto98 <diegisniet@hotmail.com>
2020-03-30 02:25:57 -0500

    Mejoras a la interfaz y el funcionamiento

--- controles.js
+++ controles.js
@@ -1,6 +1,12 @@
 var product = [
-	{id: 1, name: 'prod name 1'},
-	{id: 2, name: 'prod name 2'},
+	{	
+		id: 1, 
+		name: 'prod name 1'
+	},
+	{	
+		id: 2, 
+		name: 'prod name 2'
+	},
 ];
 
 var  coupon =[
@@ -23,21 +29,6 @@ var  coupon =[
 
 ];
 
-var detailsC =[	
-{
-	id: 1, //number
-	name: 'coupon name', // string
-	description: 'coupon description', // string
-	product_id: 1,  // number (product id)
-},
-{
-	id: 2, //number
-	name: 'coupon name 2', // string
-	description: 'coupon description 2', // string
-	product_id: 2,  // number (product id)
-}
-];
- 
 const index = (req,res,next) => {
 	res.render('index',{
 	});
@@ -56,10 +47,9 @@ const getCoupons = (req, res, next) => {
 };
 
 const detailsProducts = (req, res, next) => {
-	
 	res.render('details-products', {
 	});
-	const productid = req.body.productid;
+	
 };
 
 const detailsCoupons = (req, res, next) => {
@@ -70,18 +60,16 @@ const detailsCoupons = (req, res, next) => {
 
 
 const addProduct = (req, res) => {
-
-	const newname = req.body.newname;
-
-	product.push({
-		id: product.length + 1,
-		name: newname
-	});
 	res.render('newproduct',{
 		product : product
-	})
+	});
+};
 
-	res.redirect('/newp');
+const addCoupon = (req, res) => {
+
+	res.render('newcoupons', {
+		coupon : coupon
+	});
 };
 
 const validarCoupon = (req, res) => {
@@ -90,52 +78,52 @@ const validarCoupon = (req, res) => {
 };
 
 const vcoupon =(req,res) => {
-	res.render('vcoupon',{
-	})
-}
-const addCoupon = (req, res, next) => {
-	var newName = req.body.newName;
-	var newDescription = req.body.newDescription;
-	var newId = req.body.newId;
+	res.redirect('/validarCoupon');
+};
 
-	coupon.push = {
+function sumarDias(fecha, dias){
+	fecha.setDate(fecha.getDate() + dias);
+	return fecha;
+  }
+
+const viewCoupon = (req,res) => {
+
+	const newName = req.body.newName;
+    const newDescription = req.body.newDescription;
+	const newId = req.body.newId;
+	var newSince = new Date();
+	var newUntil = sumarDias(newSince, +10);
+
+	coupon.push({
 		id : coupon.length + 1,
 		name : newName,
 		description : newDescription,
-		product_id: newId, 
-		validsince: new Date() ,// string datetime ISO8601 format
-		validuntil: '2021-02-02 T23:59:00.999' // string datetime ISO8601 format
-	}
-	res.render('newcoupons', {
-		coupon 
+		product_id : newId,
+		valid_since: newSince,
+		valid_until : newUntil 
 	});
 
-	res.redirect('/newc');
+	res.redirect('/newcoupons')
 };
 
-const viewCoupon = (req,res,next) => {
-	res.render('newc', {
-		coupon 
+const viewProduct = (req,res) => {
+	const newname = req.body.newname;
+	console.log(newname);
+	product.push({
+		id : product.length + 1,
+		name : newname
 	});
-}
 
-const viewProuduct = (req,res,next) => {
-	res.render('newp', {
-		product
-	});
-}
+	res.redirect('/newproduct');
+};
 
-const findproduct = (req,res,next) => {
-	const indices = req.body.indeces;
-	res.render('findproduct'),{
-		indices,
-		product
-	}
+const findproduct = (req,res) => {
+	const productid = req.body.productid;
+	res.redirect('/details-products');
 };
 
 const findcoupon = (req,res) => {
-	res.render('findcoupon',{
-	})
+	res.redirect('/details-coupons');
 };
 
 module.exports = {
@@ -150,6 +138,6 @@ module.exports = {
   viewCoupon,
   findproduct,
   findcoupon,
-  viewProuduct,
+  viewProduct,
   vcoupon
 };
--- routes.js
+++ routes.js
@@ -9,24 +9,24 @@ router.get('/products', indexControles.getProducts);
 
 router.get('/details-products', indexControles.detailsProducts);
 
-router.get('/details-products/findproduct', indexControles.findproduct);
+router.post('/details-products/findproduct', indexControles.findproduct);
 
 router.get('/newproduct', indexControles.addProduct);
 
-router.get('/newproduct/newp', indexControles.viewProuduct);
+router.post('/newproduct/newp', indexControles.viewProduct);
 
 router.get('/newcoupons', indexControles.addCoupon);
 
-router.get('/newcoupons/newc', indexControles.viewCoupon);
+router.post('/newcoupons/newc', indexControles.viewCoupon);
 
 router.get('/coupons', indexControles.getCoupons);
 
 router.get('/details-coupons', indexControles.detailsCoupons);
 
-router.get('/details-coupons/findcoupon', indexControles.findcoupon);
+router.post('/details-coupons/findcoupon', indexControles.findcoupon);
 
 router.get('/validarCoupon', indexControles.validarCoupon);
 
-router.get('/validarCoupon/vcoupon', indexControles.vcoupon);
+router.post('/validarCoupon/vcoupon', indexControles.vcoupon);
 
 module.exports = router;
